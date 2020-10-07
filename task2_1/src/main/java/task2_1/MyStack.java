ba1c9572ea4efd0f57285e502f1326c8d5cffa39
Игорь Чиёсов <i.chiesov@medved.studio>
2020-10-07 19:44:51 +0700

    task2_1 Added stack realisation.

--- task2_1/src/main/java/task2_1/MyStack.java
+++ task2_1/src/main/java/task2_1/MyStack.java
@@ -1,5 +1,57 @@
 package task2_1;
 
-public class MyStack {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class MyStack<T> implements Iterable<T>{
+    private ArrayList<T> list;
+
+    public MyStack(){
+        list = new ArrayList<T>();
+    }
+
+    public void push(T elem){
+        list.add(elem);
+    }
+
+    public T pop(){
+        if(list.isEmpty()){
+            return null;
+        }
+
+        return list.remove(list.size() - 1);
+    }
+
+    public T top() throws  IndexOutOfBoundsException{
+        if(list.isEmpty()){
+            return null;
+        }
+
+        return list.get(list.size() - 1);
+    }
+
+    public int getSize(){
+        return list.size();
+    }
+
+    @Override
+    public Iterator<T> iterator(){
+        return new Iterator<T>() {
+            int pos = list.size() - 1;
+
+            @Override
+            public boolean hasNext() {
+                return pos >= 0;
+            }
+
+            @Override
+            public T next() {
+                return hasNext() ? list.get(pos--) : null;
+            }
+        };
+    }
 }
+
+
+
+
--- task2_1/src/test/java/task2_1/MyStackTest.java
+++ task2_1/src/test/java/task2_1/MyStackTest.java
@@ -1,7 +1,23 @@
 package task2_1;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyStackTest {
+    @Test
+    public void Test1(){
+        var myLittleStack = new MyStack<Object>();
+        myLittleStack.push("122");
+        myLittleStack.push("222");
+        myLittleStack.push("322");
 
+        myLittleStack.pop();
+        myLittleStack.pop();
+
+        assertEquals(myLittleStack.top(), "122");
+        assertEquals(myLittleStack.getSize(), 1);
+        assertEquals(myLittleStack.pop(), "122");
+        assertEquals(myLittleStack.getSize(), 0);
+
+    }
 }
