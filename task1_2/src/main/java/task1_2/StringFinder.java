c39a1b8d9355fe065e43adb3329a8d6214ef9afc
Игорь Чиёсов <i.chiesov@medved.studio>
2020-10-08 17:44:26 +0700

    task1_2 fixed issues with data structures.

--- task1_2/src/main/java/task1_2/StringFinder.java
+++ task1_2/src/main/java/task1_2/StringFinder.java
@@ -3,46 +3,56 @@ package task1_2;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 
 public class StringFinder {
-    public static HashSet<Long> Find(String sample, InputStream inputStream) throws IOException {
+    public static ArrayList<Long> Find(String sample, InputStream inputStream) throws IOException {
         var prefixFunc = prefixFunction(sample);
 
-            var buffSize = sample.length() * 4;
-            var buffer = new char[buffSize];
-            var buffOffset = 0;
-            var buffCapacity = buffer.length;
+        var buffSize = sample.length() * 4;
+        var buffer = new char[buffSize];
+        var buffOffset = 0;
+        var buffCapacity = buffer.length;
 
-            var result = new HashSet<Long>();
+        var result = new ArrayList<Long>();
 
-            int runCounter = 0;
-            var fileReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        int runCounter = 0;
+        var fileReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
 
-            // считывание текста в буффер
-            while (fileReader.read(buffer, buffOffset, buffCapacity) != -1) {
-                var inFileOffset = runCounter * (long)buffOffset;
+        // считывание текста в буффер
+        while (fileReader.read(buffer, buffOffset, buffCapacity) != -1) {
+            var inFileOffset = runCounter * (long) buffCapacity;
 
-                result.addAll(KMPSearch(buffer, sample, inFileOffset, prefixFunc));
+            var tmp = KMPSearch(buffer, sample, inFileOffset, prefixFunc);
 
-                buffOffset = buffer.length / 2;
-                buffCapacity = buffOffset;
-
-                // выполняем сдвиг элементов буффера влево, начиная с середины
-                // далее будем читать с середины буффера
-                // чтобы не потерять вхождения
-                for(int i = 0; i < buffCapacity; i++){
-                    buffer[i] = buffer[i + buffOffset];
-                    buffer[i + buffOffset] = '\0';
-                }
-
-                // счетчик чтений из файла
-                runCounter++;
+            // проверяем на дубликаты
+            if(result.size()>0 && (tmp.get(0) == result.get(result.size()-1))){
+                tmp.remove(0);
             }
 
-            return result;
+            result.addAll(tmp);
 
+            buffOffset = buffer.length / 4;
+            buffCapacity = buffSize - buffOffset;
+
+            // выполняем сдвиг элементов буффера влево, начиная с середины
+            // далее будем читать с 1/4 буффера
+            // чтобы не потерять вхождения
+            for (int i = 0; i < buffOffset; i++) {
+                buffer[i] = buffer[buffSize - buffCapacity + i];
+            }
+
+            for (int i = buffOffset; i < buffSize; i++) {
+                buffer[i] = '\0';
+            }
+
+            // счетчик чтений из файла
+            runCounter++;
+        }
+
+        return result;
     }
 
     private static int[] prefixFunction(String sample) {
@@ -73,7 +83,7 @@ public class StringFinder {
             }
             // образец полностью совпал с какой-то частью текста
             if (j == sample.length()) {
-                result.add(inFileOffset + (long)(i - j));
+                result.add(inFileOffset + (long) (i - j));
                 j = prefixFunc[j - 1];
             }// образец не совпал
             else if ((i < textBuff.length) && (sample.charAt(j) != textBuff[i])) {
