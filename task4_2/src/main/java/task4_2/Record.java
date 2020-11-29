081af5816eef12f59c2397e0b105b84195a1f8dc
Игорь Чиёсов <i.chiesov@medved.studio>
2020-11-29 17:38:09 +0700

    task4_2 Add documentation to Record class.

--- task4_2/src/main/java/task4_2/Record.java
+++ task4_2/src/main/java/task4_2/Record.java
@@ -9,6 +9,11 @@ public class Record {
     private Date createdAt;
     private Date updatedAt;
 
+    /**
+     * Конструктор класса Record
+     * @param title - заголовок записи
+     * @param content - содержимое записи
+     */
     public Record(String title, String content) {
         this.title = title;
         this.content = content;
@@ -16,18 +21,32 @@ public class Record {
         updatedAt = (Date) this.createdAt.clone();
     }
 
+    /**
+     * @return заголовок текущей записи
+     */
     public String getTitle() {
         return this.title;
     }
 
+    /**
+     * @return содержимое текущей записи
+     */
     public String getContent() {
         return this.content;
     }
 
+    /**
+     * @return дата создания текущей записи
+     */
     public Date getCreatedAtDate() {
         return this.createdAt;
     }
 
+    /**
+     * Проверяет, содержит ли заголовок текущей записи ключевые слова
+     * @param keyWords - массив ключевых слов
+     * @return true/false
+     */
     public boolean containsKeyWords(String[] keyWords) {
         var title = this.title.toLowerCase();
         for (var keyWord : keyWords) {
