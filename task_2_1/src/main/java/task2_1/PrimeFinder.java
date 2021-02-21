cd415a3886156a6c295590a736c861ebca2364ce
Игорь Чиёсов <i.chiesov@medved.studio>
2021-02-21 17:11:47 +0700

    Add concurrent primes checking on threads.

--- task_2_1/src/main/java/task2_1/PrimeFinder.java
+++ task_2_1/src/main/java/task2_1/PrimeFinder.java
@@ -1,10 +1,16 @@
 package task2_1;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static java.lang.Thread.sleep;
 
 public class PrimeFinder {
     /**
      * Последовательно проверяет все числа, полученного на вход списка целых чисел, на простоту
+     *
      * @param numbers - список целых чисел
      * @return true - если список содержит хотя-бы одно простое число
      * false - в противном случае
@@ -13,8 +19,37 @@ public class PrimeFinder {
         return numbers.stream().anyMatch(n -> this.isPrime(n));
     }
 
+    public boolean hasPrimesOnThreads(ArrayList<Integer> numbers, int threadCnt) throws InterruptedException {
+        var hasPrimes = new AtomicBoolean(false);
+        var threadsLimit = new AtomicInteger(threadCnt);
+
+        numbers.stream().forEach(n -> {
+            while (threadsLimit.get() <= 0) {
+                System.out.println("waiting...");
+            }
+            threadsLimit.getAndDecrement();
+            new Thread(() -> {
+                var isPrime = this.isPrime(n);
+                System.out.println(n + " = " + isPrime);
+                hasPrimes.compareAndSet(false, isPrime);
+
+                threadsLimit.getAndIncrement();
+            }).start();
+        });
+
+        while (threadsLimit.get() != threadCnt) {
+            System.out.println("waiting...");
+        }
+
+        return hasPrimes.get();
+    }
+
     private boolean isPrime(int x) {
-        for (int i = 2; i*i <= x;i++){
+        if (x < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= x; i++) {
             if (x % i == 0) {
                 return false;
             }
--- task_2_1/src/test/java/task2_1/PrimeFinderTest.java
+++ task_2_1/src/test/java/task2_1/PrimeFinderTest.java
@@ -14,4 +14,11 @@ class PrimeFinderTest {
         var numbers = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
         assertEquals(finder.hasPrimesSequent(numbers), true);
     }
+
+    @Test
+    public void TestThreadFinder() throws InterruptedException {
+        var finder = new PrimeFinder();
+        var numbers = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertEquals(finder.hasPrimesOnThreads(numbers, 6), true);
+    }
 }
